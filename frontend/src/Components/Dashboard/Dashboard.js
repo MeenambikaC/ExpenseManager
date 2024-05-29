536e97a1588b418fdef7ab59388d8b205356e202
MeenambikaC <c.meenambika@gmail.com>
2024-05-30 00:52:58 +0530

    Adding Chart to Dashboard

--- frontend/src/Components/Dashboard/Dashboard.js
+++ frontend/src/Components/Dashboard/Dashboard.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import styled from 'styled-components';
 import { InnerLayout } from '../../Styles/layouts';
+import Chart from '../Chart/Chart';
 
 function Dashboard() {
   return (
     <div>
       <DashboardStyled>
         <InnerLayout>
-          Dashboard
+          <h1>All Transcations</h1>
+          <div className="stats-con">
+            <div className="chart-con">
+              <Chart/>
+            </div>
+          </div>
         </InnerLayout>
       </DashboardStyled>
     </div>
