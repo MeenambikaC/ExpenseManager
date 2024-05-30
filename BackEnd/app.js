8bcca1f8f0c54e11e7b584b5d153363633229c8e
MeenambikaC <c.meenambika@gmail.com>
2024-05-31 00:10:43 +0530

    ChangingReadSYNC

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -3,7 +3,8 @@ const cors= require('cors')
 const { db } = require('./db/db')
 const {readdirSync}=require('fs')
 const app =express()
-
+const fs = require('fs');
+const path = require('path');
 require ('dotenv').config()
 const PORT=process.env.PORT
 
@@ -15,7 +16,20 @@ app.get('/', (req,res)=>{
     res.send("Hello World!")
 })
 // read files inside routes folder
-readdirSync('.\\routes').map((route)=>app.use('/api/v1', require('./routes/'+ route)))
+// readdirSync('.\\routes').map((route)=>app.use('/api/v1', require('./routes/'+ route)))
+// Dynamically load routes from files inside the routes directory
+const routesDir = path.join(__dirname, 'routes');
+fs.readdir(routesDir, (err, files) => {
+    if (err) {
+        console.error('Error loading routes:', err);
+        return;
+    }
+    files.forEach((file) => {
+        const routePath = path.join(routesDir, file);
+        const routeHandler = require(routePath);
+        app.use('/api/v1', routeHandler);
+    });
+});
 const server =()=>{
     db()
     app.listen(PORT,()=>{
