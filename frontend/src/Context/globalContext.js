e2326bc697d36d04771d11e7d6580749281b34f5
MeenambikaC <c.meenambika@gmail.com>
2024-06-01 01:35:14 +0530

    Updating base url to vercel

--- frontend/src/Context/globalContext.js
+++ frontend/src/Context/globalContext.js
@@ -2,9 +2,9 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-// const BASE_URL = "https://expense-manager-vj1a-git-main-meenambikacs-projects.vercel.app/api/v1/";
+const BASE_URL = "https://expense-manager-vj1a-git-main-meenambikacs-projects.vercel.app/api/v1/";
 
-const BASE_URL="http://localhost:5000/api/v1/"
+// const BASE_URL="http://localhost:5000/api/v1/"
 const GlobalContext = React.createContext()
 
 export const GlobalProvider = ({children}) => {
