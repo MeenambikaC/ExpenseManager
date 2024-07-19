b849eb5a8688997f2d2202e9f9764c0e34c57234
MeenambikaC <c.meenambika@gmail.com>
2024-07-19 16:58:02 +0530

    cron update

--- BackEnd/api/cron.js
+++ BackEnd/api/cron.js
@@ -1,11 +1,15 @@
 const sendRemindersForToday = require('../controller/reminder').sendRemindersForToday;
-const cron = require('node-cron');
 
-cron.schedule('*/3 * * * *', async () => {
+export default async function handler(req, res) {
     try {
+        // const now = new Date();
+        // const srilankanTime = new Date(now.toLocaleString("en-US", { timeZone: "Asia/Colombo" }));
+        // const dateToCheck = srilankanTime.toISOString().split('T')[0];
+
         await sendRemindersForToday();
-        console.log('Reminder emails sent successfully');
+        res.status(200).json({ message: `Reminder emails sent successfully for: ${dateToCheck}` });
     } catch (error) {
         console.error('Error sending reminder emails:', error);
+        res.status(500).json({ error: 'Failed to send reminder emails' });
     }
-});
+}
