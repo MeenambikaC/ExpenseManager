25bb26d0ab2fc4659151226e380654804d825d97
MeenambikaC <c.meenambika@gmail.com>
2024-07-19 16:32:22 +0530

    changing cron logic

--- BackEnd/api/cron.js
+++ BackEnd/api/cron.js
@@ -1,15 +1,11 @@
 const sendRemindersForToday = require('../controller/reminder').sendRemindersForToday;
+const cron = require('node-cron');
 
-export default async function handler(req, res) {
+cron.schedule('*/3 * * * *', async () => {
     try {
-        // const now = new Date();
-        // const srilankanTime = new Date(now.toLocaleString("en-US", { timeZone: "Asia/Colombo" }));
-        // const dateToCheck = srilankanTime.toISOString().split('T')[0];
-
         await sendRemindersForToday();
-        res.status(200).json({ message: `Reminder emails sent successfully for: ${dateToCheck}` });
+        console.log('Reminder emails sent successfully');
     } catch (error) {
         console.error('Error sending reminder emails:', error);
-        res.status(500).json({ error: 'Failed to send reminder emails' });
     }
-}
+});
